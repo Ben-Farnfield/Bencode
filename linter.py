696924fda201e47785b1c424dfb5eb5b7f740196
Ben Farnfield <ben.farnfield@gmail.com>
2013-12-19 09:57:24 +0000

    Added fa for integers

--- linter.py
+++ linter.py
@@ -1,2 +1,31 @@
-def lint(bcode):
+#! /usr/bin/python
+
+import re
+
+def int_fa(bcode, i):
+
+    table = {'A' : {'i':'B'},
+             'B' : {'\d':'C'},
+             'C' : {'\d':'D', 'e':None},
+             'D' : {'\d':'D', 'e':None}}
+
+    state = 'A'
+    while state is not None:
+        char = bcode[i]
+        if char.isdigit():
+            char = '\d'
+        try:
+            state = table[state][char]
+            i += 1
+        except KeyError:
+            return (False, i)
+    return (True, i)
+
+def str_fa(bcode, i):
     pass
+
+def lint(bcode):
+    print int_fa(bcode, 0)
+
+lint('i1000000e')
+lint('5:hello')
