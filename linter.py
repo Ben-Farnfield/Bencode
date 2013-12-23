2c2f5a9516869ce7afbb4e6f0c77e1ee16318b6c
Ben Farnfield <ben.farnfield@gmail.com>
2013-12-23 23:00:08 +0000

    Changed val_bint and val_bstr so they now use regex. Implemented val_blist to use and recursive FA

--- linter.py
+++ linter.py
@@ -2,30 +2,85 @@
 
 import re
 
-def int_fa(bcode, i):
+def val_bint(bcode, pointer):
 
-    table = {'A' : {'i':'B'},
-             'B' : {'\d':'C'},
-             'C' : {'\d':'D', 'e':None},
-             'D' : {'\d':'D', 'e':None}}
+    m = re.compile(r"(i\d+e)").match(bcode, pointer)
+    if m is None:
+        return {'result':False, 'pointer':pointer} # not a bencoded int
+    return {'result':True, 'pointer':m.end(m.lastindex)}
+
+
+def val_bstr(bcode, pointer):
+
+    m = re.compile(r"(?P<str_len>\d+)(?P<str_start>:)").match(bcode, pointer)
+    if m is None:
+        return {'result':False, 'pointer':pointer} # not a bencoded string
+    str_len = int(m.group('str_len'))
+    str_start = m.end('str_start')
+    str_end = str_start + str_len
+    if str_end <= len(bcode):
+        return {'result':True, 'pointer':str_end} 
+    else:
+        return {'result':False, 'pointer':pointer}
+
+
+def val_blist(bcode, pointer):
+
+    table = {'A' : {'l':'B'},
+             'B' : {'bint':'C','bstr':'D','blist':'E','bdict':'F'},
+             'C' : {'bint':'C','bstr':'D','blist':'E','bdict':'F'},
+             'D' : {'bint':'C','bstr':'D','blist':'E','bdict':'F'},
+             'E' : {'bint':'C','bstr':'D','blist':'E','bdict':'F'},
+             'F' : {'bint':'C','bstr':'D','blist':'E','bdict':'F'}}
 
     state = 'A'
-    while state is not None:
-        char = bcode[i]
-        if char.isdigit():
-            char = '\d'
+    inside_blist = False
+
+    while True:
+        try:
+            char = bcode[pointer]
+        except IndexError:
+            return {'result':False, 'pointer':pointer}
+
+        if char == 'l' and inside_blist == False:
+            pointer += 1
+            inside_blist = True
+        elif char != 'l' and inside_blist == False:
+            return {'result':False, 'pointer':pointer}
+        elif char == 'e' and inside_blist == True:
+            pointer += 1
+            return {'result':True, 'pointer':pointer}
+        else:
+            bint = val_bint(bcode, pointer)
+            bstr = val_bstr(bcode, pointer)
+            blist = val_blist(bcode, pointer)
+            bdict = val_bdict(bcode, pointer)
+
+            if bint['result']:
+                char = 'bint'
+                pointer = bint['pointer']
+            elif bstr['result']:
+                char = 'bstr'
+                pointer = bstr['pointer']
+            elif blist['result']:
+                char = 'blist'
+                pointer = blist['pointer']
+            elif bdict['result']:
+                char = 'bdict'
+                pointer = bdict['pointer']
+            else:
+                pass # RAISE ERROR
+
         try:
             state = table[state][char]
-            i += 1
         except KeyError:
-            return (False, i)
-    return (True, i)
+            return {'result':False, 'pointer':pointer}
 
-def str_fa(bcode, i):
+
+def val_bdict(bcode, pointer):
+    return {'result':False, 'pointer':pointer}
+
+
+def val_bencode(bcode):
     pass
 
-def lint(bcode):
-    print int_fa(bcode, 0)
-
-lint('i1000000e')
-lint('5:hello')
